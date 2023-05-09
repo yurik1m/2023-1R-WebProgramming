6893ae28f1d6441ff976e78a65aaefc7c8d34ad5
yurik1m <yuri.k.tech@gmail.com>
2023-05-09 21:27:55 +0900

    [23-05-09]useEffect 실습

--- 2023-1R-WebProgramming/src/App.jsx
+++ 2023-1R-WebProgramming/src/App.jsx
@@ -4,7 +4,7 @@
 //우리는 리액트가 어떻게 언제 몇 번 실행될 지 예상하기 어려움. 그렇기 때문에 App()는 항상 pure function이여야 함
 //프로그램 함수의 결과가 매개변수를 통해서만 바뀌는 것.
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -16,26 +16,38 @@ function App() {
   // const [count, setCount] = useState(0) //훅
   const [row, setRow] = useState([]);
   const [isLoad, setIsLoad] = useState(false);
-
-  function load() {
-    if(row.length == 0){
-    const res = fetch("http://openapi.seoul.go.kr:8088/71747673467a657235396c54657648/json/RealtimeCityAir/1/25/").then(
-      function(res2){
-        res2.json().then( function(res3){
-          setRow(res3.RealtimeCityAir.row);
-        });
-      });
-  }
-   
-  }
+  const [count, setCount] = useState(0);
   
-  function makeTable() {
-    setIsLoad(!isLoad);
-    if(isLoad) {
-      load();
-    }
-  }
+  useEffect(() => {
+    console.log('mount or update');
+    return () => console.log('unmount(update ready)');
+    
+  });
 
+  useEffect(() => {
+    console.log('mount only');
+      if(row.length == 0){
+      const res = fetch("http://openapi.seoul.go.kr:8088/71747673467a657235396c54657648/json/RealtimeCityAir/1/25/").then(
+        function(res2){
+          res2.json().then( function(res3){
+            setRow(res3.RealtimeCityAir.row);
+          });
+        });
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log('row only');
+  },[row]);
+//모든 컴포넌트의 업데이트를 두 번씩 실행해줌 
+  
+useEffect(() => {
+  document.title = `You clicked ${count} times`;
+  console.log({count})
+  return () => {
+    document.title = "vite + React";
+  };
+}, [count]);
   return (
     <>
       <div>
@@ -47,25 +59,25 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <button style={buttonStyle} onClick={makeTable}>테이블 생성</button>
+      <button style={buttonStyle} onClick={() => setIsLoad(!isLoad)}>테이블 생성</button>
       {isLoad && (
       <table style={tableStyle}>
         <thead>
           <tr>
-            <th>구이름</th>
-            <th>PM10</th>
-            <th>O3</th>
-            <th>상태</th>
+            <th style={thStyle}>구이름</th>
+            <th style={thStyle}>PM10</th>
+            <th style={thStyle}>O3</th>
+            <th style={thStyle}>상태</th>
           </tr>
           </thead>
           <tbody>
-            {row.map(function(obj, index) {
+            {row.map((obj, index) => {
               return (
                 <tr key={index}>
-                  <td>{obj.MSRSTE_NM}</td>
-                  <td>{obj.PM10}</td>
-                  <td>{obj.O3}</td>
-                  <td>{obj.IDEX_NM}</td>
+                  <td style={tdStyle}>{obj.MSRSTE_NM}</td>
+                  <td style={tdStyle}>{obj.PM10}</td>
+                  <td style={tdStyle}>{obj.O3}</td>
+                  <td style={tdStyle}>{obj.IDEX_NM}</td>
                 </tr>
               )
             })}
@@ -74,11 +86,12 @@ function App() {
         
       </table>
       )}
-       {/*<div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+       <div className="card">
+        <button style={buttonStyle} onClick={() => setCount((count) => count + 1)}>
+          Count 증가
         </button>
-        <p>
+      </div>
+       {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
@@ -97,12 +110,23 @@ const tableStyle = {
   borderCollapse: "collapse",
   textAlign: "center",
   margin: "auto",
-  width: "300px"
+  width: "400px"
 };
 
 const buttonStyle = {
   border: "1px solid green",
   width: "150px",
   height: "50px",
-  marginBottom: "50px"
+  marginBottom: "50px",
+  backgroundColor: "#C8E6C9"
+}
+
+const tdStyle = {
+  border: "1px solid gray",
+  width: "100px",
+}
+
+const thStyle = {
+  backgroundColor: "#C8E6C9",
+  border: "1px solid gray"
 }
